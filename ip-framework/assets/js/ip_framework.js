d20a9043c55f27a0399417a8db63d52139ff24f9
amanda-inverseparadox <amanda@inverseparadox.net>
2013-02-20 12:43:08 -0500

    minor updates to widgets & image sizes

--- ip-framework/assets/js/ip_framework.js
+++ ip-framework/assets/js/ip_framework.js
@@ -28,53 +28,53 @@ jQuery(function($){
 	$('.close-options').click(function(e){
 		e.preventDefault();
 		var container = $(this).parents('.broken-table-row');
-		
+
 		var label = container.find('.field-label input').val();
 		container.find('.ipf-options td.label .row-title').html(label);
-		
+
 		var name = container.find('.field-name input').val();
 		container.find('.ipf-options td.name').html(name);
-		
+
 		var type = container.find('.field-type select').val();
 		container.find('.ipf-options td.type').html(type);
-		
+
 		var order = container.find('.field-order input').val();
 		container.find('.ipf-options td.order').html(order);
-		
+
 		container.find('.field-options').hide();
-		
+
 	});
 	$('.close-options-cpt').click(function(e){
 		e.preventDefault();
 		var container = $(this).parents('.broken-table-row');
-		
+
 		var name = container.find('.field-post_type input').val();
 		container.find('.ipf-options td.label .row-title').html(name);
 
 		var label = container.find('.field-label-name input').val();
 		container.find('.ipf-options td.name').html(label);
-		
+
 		var type = container.find('.field-args-capability_type select').val();
 		container.find('.ipf-options td.type').html(type);
-		
+
 		var desc = container.find('.field-args-description textarea').val();
 		container.find('.ipf-options td.description').html(desc);
-		
+
 		container.find('.field-options').hide();
 	});
 	$('.close-options-ctax').click(function(e){
 		e.preventDefault();
 		var container = $(this).parents('.broken-table-row');
-		
+
 		var name = container.find('.field-taxonomy_name input').val();
 		container.find('.ipf-options td.name .row-title').html(name);
 
 		var label = container.find('.field-label-name input').val();
 		container.find('.ipf-options td.label').html(label);
-		
+
 		var hierarchcial = container.find('.field-args-hierarchcial input').val();
 		container.find('.ipf-options td.hierarchcial').html(hierarchcial);
-		
+
 		container.find('.field-options').hide();
 	});
 	$('.close-options-menus').click(function(e){
@@ -83,13 +83,46 @@ jQuery(function($){
 
 		var label = container.find('.field-label input').val();
 		container.find('.ipf-options td.label .row-title').html(label);
-		
+
 		var slug = container.find('.field-slug input').val();
 		container.find('.ipf-options td.slug').html(slug);
-		
+
 		container.find('.field-options').hide();
 	});
-		
+	$('.close-options-sizes').click(function(e){
+		e.preventDefault();
+		var container = $(this).parents('.broken-table-row');
+
+		var name = container.find('.field-name input').val();
+		container.find('.ipf-options td.name .row-title').html(name);
+
+		var width = container.find('.field-width input').val();
+		container.find('.ipf-options td.width').html(width);
+
+		var height = container.find('.field-height input').val();
+		container.find('.ipf-options td.height').html(height);
+
+		var crop = container.find('.field-crop input').val();
+		container.find('.ipf-options td.crop').html(crop);
+
+		container.find('.field-options').hide();
+	});
+	$('.close-options-sidebars').click(function(e){
+		e.preventDefault();
+		var container = $(this).parents('.broken-table-row');
+
+		var name = container.find('.field-name input').val();
+		container.find('.ipf-options td.name .row-title').html(name);
+
+		var id = container.find('.field-id input').val();
+		container.find('.ipf-options td.id').html(id);
+
+		var description = container.find('.field-description input').val();
+		container.find('.ipf-options td.description').html(description);
+
+		container.find('.field-options').hide();
+	});
+
 	$('.field-type select').change(function(){
 		if ($(this).val() == 'Select' || $(this).val() == 'Radio' || $(this).val() == 'Checkbox') {
 			$(this).parents('tbody').find('.field-options').show();
@@ -134,7 +167,7 @@ function cloneRow(origRow, appendContainer, rowClass) {
 			} else {
 				jQuery(this).find('select').attr('name', 'options['+newid+']['+name+']');
 			}
-		} 
+		}
 	});
 	var max = jQuery(appendContainer+' '+rowClass).length;
 	newrow.find('.field-order input').val(max);
--- ip-framework/controllers/controller.php
+++ ip-framework/controllers/controller.php
@@ -2,7 +2,7 @@
 class IP_Framework_Controller
 {
 	public function default_action() {
-		$controller = new IP_Framework_Controller_Setup_Theme_Options();
+		$controller = new IP_Framework_Controller_Nav_Menus();
 		$controller->default_action();
 	}
 }
